2a02b8cd0ee5e1b3ff28765d36f22b566beddba6
josevilanir <vilanirneto@hotmail.com>
2025-10-27 10:02:58 -0300

    feat: update workout list tab with latest changes
    - Sync with workout detail page improvements
    - Ensure compatibility with new loading patterns

--- lib/features/workout/pages/history/widgets/workout_list_tab.dart
+++ lib/features/workout/pages/history/widgets/workout_list_tab.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:go_router/go_router.dart';
-import '../../../../../core/constants.dart';
 import '../../../controllers/providers.dart';
 import '../../../../../data/db/app_database.dart';
 
